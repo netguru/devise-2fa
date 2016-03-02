e5ddde360ea8b7692234319d9015b42615743840
Kuba Niechciał <jakub.niechcial@netguru.co>
2016-03-02 08:30:54 +0100

    initial two_factor_auth setup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
-  devise :database_authenticatable, :registerable,
+  devise :two_factor_authenticatable, :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_one_time_password(encrypted: true)
 end
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,4 +1,9 @@
 Devise.setup do |config|
+  config.max_login_attempts = 3  # Maximum second factor attempts count.
+  config.allowed_otp_drift_seconds = 30  # Allowed time drift between client and server.
+  config.otp_length = 6  # OTP code length
+  config.otp_secret_encryption_key = ENV['OTP_SECRET_ENCRYPTION_KEY']
+
   config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
 
   require 'devise/orm/active_record'
