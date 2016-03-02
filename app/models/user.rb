1a4830a2896b1ff2a40362b0f47ef4c413cde950
Kuba Niechciał <jakub.niechcial@netguru.co>
2016-03-02 08:39:00 +0100

    add model callback to handle changing of two auth confirmation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  before_save :valid_two_factor_confirmation
+
   devise :two_factor_authenticatable, :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
@@ -6,5 +8,20 @@ class User < ActiveRecord::Base
 
   def need_two_factor_authentication?(request)
     two_factor_enabled? && !unconfirmed_two_factor?
+  end
+
+  private
+
+  def valid_two_factor_confirmation
+    return true unless two_factor_just_set || phone_changed_with_two_factor
+    self.unconfirmed_two_factor = true
+  end
+
+  def two_factor_just_set
+    two_factor_enabled? && two_factor_enabled_changed?
+  end
+
+  def phone_changed_with_two_factor
+    two_factor_enabled? && phone_changed?
   end
 end
