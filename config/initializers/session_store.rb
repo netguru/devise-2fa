628c04940b5680b50e603dcd03487045745002f2
Kuba Niechciał <jakub.niechcial@netguru.co>
2016-03-02 08:26:23 +0100

    initial devise setup

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_super-secret-puppies_session'
+Rails.application.config.session_store :cookie_store, key: '_devise-2fa_session'
