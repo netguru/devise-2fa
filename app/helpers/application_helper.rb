4398e6a38a323b56988f230a8083122ed3c1945f
Kuba Niechciał <jakub.niechcial@netguru.co>
2016-03-02 08:52:57 +0100

    add google_auth_qrcode renderer helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def google_authenticator_qrcode(user)
+    data = user.provisioning_uri
+    data = Rack::Utils.escape(data)
+    url = "https://chart.googleapis.com/chart?chs=200x200&chld=M|0&cht=qr&chl=#{data}"
+    return image_tag(url, :alt => 'Google Authenticator QRCode')
+  end
 end
