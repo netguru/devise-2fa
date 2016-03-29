d731a203888a2b197b3682b1006d91424b32f6fc
Krzysztof Dabrowski <krzysztof.dabrowski@netguru.co>
2016-03-29 15:14:24 +0200

    Creating routes for show and update

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'welcome#index'
   resources 'puppies', :only => 'index'
+  resources 'users', :only => ['show', 'update']
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
